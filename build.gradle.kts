fe4ca30ec5d5f48260247414704b4158ab77d845
Alfredo Miranda <alfredo.miranda.learning@gmail.com>
2024-12-18 16:54:51 -0600

    implemented config server

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,6 +21,8 @@ extra["springCloudVersion"] = "2023.0.3"
 
 dependencies {
 	implementation("org.springframework.cloud:spring-cloud-stream-binder-rabbit")
+	implementation("org.springframework.cloud:spring-cloud-starter-config")
+	implementation("org.springframework.retry:spring-retry")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("io.projectreactor:reactor-test")
 	testImplementation("org.springframework.cloud:spring-cloud-stream-test-binder")
@@ -35,4 +37,17 @@ dependencyManagement {
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+}
+
+tasks.bootBuildImage {
+	builder.set("docker.io/paketobuildpacks/builder-jammy-base")
+	imageName = "${project.name}"
+	environment = mapOf("BP_JVM_VERSION" to "21")
+	docker {
+		publishRegistry {
+			username = project.findProperty("registryUsername").toString()
+			password = project.findProperty("registryToken").toString()
+			url = project.findProperty("registryUrl").toString()
+		}
+	}
 }
